c5ef761aa86ae48fa41bbc760d2b023486946f09
Yusuf-Patel7 <Yusuf.Patel@ada.ac.uk>
2024-10-11 15:14:11 +0000

    Final Complete

--- rps.js
+++ rps.js
@@ -1,23 +1,23 @@
 function rockPaperScissors(player1, player2) {
   if (player1 === player2) {
     return "draw";
-  } else if (
-    (player1 === "rock" && player2 === "scissors")
-    || (player1 === "scissors" && player2 === "paper")
-    || player1 === "paper" && player2 === "rock"
-    || player1 === "rock" && player2 === "lizard"
-    || player1 === "scissors" && player2 === "lizard"
-    || player1 === "lizard" && player2 === "paper"
-    || player1 === "spock" && player2 === "rock"
-    || player1 === "spock" && player2 === "scissors"
-    || player1 === "lizard" && player2 === "spock"
-    || player1 === "paper" && player2 === "spock"
-  ) {
+  }
+
+  const winConditions = {
+    rock: ["scissors", "lizard"],
+    scissors: ["paper", "lizard"],
+    paper: ["rock", "spock"],
+    lizard: ["paper", "spock"],
+    spock: ["rock", "scissors"] 
+  };
+
+  if (winConditions[player1].includes(player2)) {
     return "player1";
-  } 
+  }
+
   return "player2";
-  
 }
+
 
 
 
